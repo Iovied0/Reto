04cf86e5a43da0880227f54846e88feae0618757
Yeray <yeray.fermosomo@elorrieta-errekamari.com>
2025-02-13 01:41:15 +0100

    dejar panel nuevoViaje listo para que ivan haga el Insert en la BBDD

--- RETO2-DAM/PROGRAM/Reto2/src/agenciaViajes/vista/paneles/NuevoViaje.java
+++ RETO2-DAM/PROGRAM/Reto2/src/agenciaViajes/vista/paneles/NuevoViaje.java
@@ -8,12 +8,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
+import java.sql.Date;
 import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -33,179 +34,190 @@ import agenciaViajes.controlador.Controlador;
 
 public class NuevoViaje {
 
-    public JPanel panel;
-    public Controlador controlador;
+	public JPanel panel;
+	public Controlador controlador;
 
-    public NuevoViaje(ArrayList<JPanel> paneles, ViajesErrekamari frame) {
-        panel = new JPanel();
-        panel.setBounds(0, 0, 1300, 900);
-        panel.setLayout(null);
+	public NuevoViaje(ArrayList<JPanel> paneles, ViajesErrekamari frame) {
+		panel = new JPanel();
+		panel.setBounds(0, 0, 1300, 900);
+		panel.setLayout(null);
 
-        controlador = Controlador.getInstanceControlador();
+		controlador = Controlador.getInstanceControlador();
 
-        JLabel labelTitulo = new JLabel("INTRODUZCA LOS PARÁMETROS DEL NUEVO VIAJE");
-        labelTitulo.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 20));
-        labelTitulo.setBounds(15, 20, 600, 25);
-        panel.add(labelTitulo);
+		JLabel labelTitulo = new JLabel("INTRODUZCA LOS PARÁMETROS DEL NUEVO VIAJE");
+		labelTitulo.setFont(new Font("Lucida Sans Unicode", Font.BOLD, 20));
+		labelTitulo.setBounds(15, 20, 600, 25);
+		panel.add(labelTitulo);
 
-        JLabel labelNombre = new JLabel("Nombre del viaje");
-        labelNombre.setBounds(30, 100, 150, 25);
-        panel.add(labelNombre);
+		JLabel labelNombre = new JLabel("Nombre del viaje");
+		labelNombre.setBounds(30, 100, 150, 25);
+		panel.add(labelNombre);
 
-		JComboBox<String> comboViaje = new JComboBox<>();
 		ArrayList<Viaje> viajes = controlador.getViajes();
-		for (Viaje viaje : viajes) {
-			comboViaje.addItem(viaje.getNombreViaje());
+
+		JTextField txtNombreViaje = new JTextField();
+		txtNombreViaje.setBounds(175, 100, 150, 25);
+		panel.add(txtNombreViaje);
+
+		JLabel labelDescripcion = new JLabel("Descripción del viaje");
+		labelDescripcion.setBounds(30, 396, 150, 25);
+		panel.add(labelDescripcion);
+
+		JTextArea textDescripcion = new JTextArea();
+		textDescripcion.setBounds(175, 396, 400, 100);
+		textDescripcion.setLineWrap(true); // Salto de línea automático
+		panel.add(textDescripcion);
+
+		JLabel labelFechaInicio = new JLabel("Fecha de inicio");
+		labelFechaInicio.setBounds(30, 209, 150, 25);
+		panel.add(labelFechaInicio);
+
+		UtilDateModel modelInicio = new UtilDateModel();
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanelInicio = new JDatePanelImpl(modelInicio, p);
+		JDatePickerImpl datePickerInicio = new JDatePickerImpl(datePanelInicio, new DateLabelFormatter());
+		datePickerInicio.setBounds(175, 209, 150, 25);
+		panel.add(datePickerInicio);
+
+		JLabel labelFechaFin = new JLabel("Fecha de fin");
+		labelFechaFin.setBounds(30, 259, 150, 25);
+		panel.add(labelFechaFin);
+
+		UtilDateModel modelFin = new UtilDateModel();
+		JDatePanelImpl datePanelFin = new JDatePanelImpl(modelFin, p);
+		JDatePickerImpl datePickerFin = new JDatePickerImpl(datePanelFin, new DateLabelFormatter());
+		datePickerFin.setBounds(175, 259, 150, 25);
+		panel.add(datePickerFin);
+
+		JLabel labelDuracion = new JLabel("Duración (días)");
+		labelDuracion.setBounds(30, 303, 150, 25);
+		panel.add(labelDuracion);
+
+		JTextField textDuracion = new JTextField();
+		textDuracion.setBounds(175, 303, 150, 25);
+		textDuracion.setEditable(false);
+		panel.add(textDuracion);
+
+		ChangeListener changeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				java.util.Date fechaInicioUtil = modelInicio.getValue();
+				java.util.Date fechaFinUtil = modelFin.getValue();
+
+				if (fechaInicioUtil != null && fechaFinUtil != null) {
+					long duracion = (fechaFinUtil.getTime() - fechaInicioUtil.getTime()) / (1000 * 60 * 60 * 24);
+					textDuracion.setText(String.valueOf(duracion));
+				} else {
+					textDuracion.setText("");
+				}
+			}
+		};
+
+		datePickerInicio.getModel().addChangeListener(changeListener);
+		datePickerFin.getModel().addChangeListener(changeListener);
+
+		JLabel labelServicios = new JLabel("Servicios no incluidos");
+		labelServicios.setBounds(30, 525, 150, 25);
+		panel.add(labelServicios);
+
+		JTextArea textServicios = new JTextArea();
+		textServicios.setBounds(175, 525, 400, 100);
+		textServicios.setLineWrap(true); // Salto de línea automático
+		panel.add(textServicios);
+
+		JLabel labelTipoViaje = new JLabel("Tipo de viaje");
+		labelTipoViaje.setBounds(30, 155, 150, 25);
+		panel.add(labelTipoViaje);
+
+		JComboBox<String> comboTipoViaje = new JComboBox<>();
+		ArrayList<TipoViaje> tiposViaje = controlador.getTiposViaje();
+		for (TipoViaje tipoViaje : tiposViaje) {
+			comboTipoViaje.addItem(tipoViaje.getDescripcion());
 		}
-		comboViaje.setBounds(175, 100, 150, 25);
-		panel.add(comboViaje);
-		
-        JLabel labelDescripcion = new JLabel("Descripción del viaje");
-        labelDescripcion.setBounds(30, 140, 150, 25);
-        panel.add(labelDescripcion);
+		comboTipoViaje.setBounds(175, 155, 190, 25);
+		panel.add(comboTipoViaje);
 
-        JTextArea textDescripcion = new JTextArea();
-        textDescripcion.setBounds(175, 140, 400, 100);
-        panel.add(textDescripcion);
+		JLabel labelPais = new JLabel("País destino");
+		labelPais.setBounds(30, 350, 150, 25);
+		panel.add(labelPais);
 
-        JLabel labelFechaInicio = new JLabel("Fecha de inicio");
-        labelFechaInicio.setBounds(30, 260, 150, 25);
-        panel.add(labelFechaInicio);
+		JComboBox<String> comboPais = new JComboBox<>();
+		ArrayList<Pais> paises = controlador.mostrarPaises();
+		for (Pais pais : paises) {
+			comboPais.addItem(pais.getNombre());
+		}
+		comboPais.setBounds(175, 350, 190, 25);
+		panel.add(comboPais);
 
-        UtilDateModel modelInicio = new UtilDateModel();
-        Properties p = new Properties();
-        p.put("text.today", "Today");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-        JDatePanelImpl datePanelInicio = new JDatePanelImpl(modelInicio, p);
-        JDatePickerImpl datePickerInicio = new JDatePickerImpl(datePanelInicio, new DateLabelFormatter());
-        datePickerInicio.setBounds(175, 260, 150, 25);
-        panel.add(datePickerInicio);
+		JButton btnConfirmar = new JButton("Confirmar");
+		btnConfirmar.setBounds(131, 722, 150, 25);
+		btnConfirmar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 
-        JLabel labelFechaFin = new JLabel("Fecha de fin");
-        labelFechaFin.setBounds(30, 300, 150, 25);
-        panel.add(labelFechaFin);
+				// Comprobamos que no haya campos vacíos
+				if (txtNombreViaje.getText().replace(" ", "").equals("")
+						|| textDescripcion.getText().replace(" ", "").equals("")
+						|| textServicios.getText().replace(" ", "").equals("") || modelInicio.getValue() == null
+						|| modelFin.getValue() == null || textDuracion.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Debe rellenar todos los campos", "Campos vacíos",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				// Comprobamos que el nombre del viaje no se encuentre en uso
+				for (Viaje viaje : viajes) {
+					if (txtNombreViaje.getText().equals(viaje.getNombreViaje())) {
+						JOptionPane.showMessageDialog(null, "El nombre proporcionado ya está en uso actualmente",
+								"Nombre en uso", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				}
 
-        UtilDateModel modelFin = new UtilDateModel();
-        JDatePanelImpl datePanelFin = new JDatePanelImpl(modelFin, p);
-        JDatePickerImpl datePickerFin = new JDatePickerImpl(datePanelFin, new DateLabelFormatter());
-        datePickerFin.setBounds(175, 300, 150, 25);
-        panel.add(datePickerFin);
+			}
+		});
+		panel.add(btnConfirmar);
 
-        JLabel labelDuracion = new JLabel("Duración (días)");
-        labelDuracion.setBounds(30, 340, 150, 25);
-        panel.add(labelDuracion);
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(411, 722, 150, 25);
+		btnCancelar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Boton de cancelar que lleve a donde haga falta
+				frame.gotoViajes();
+			}
+		});
+		panel.add(btnCancelar);
+	}
 
-        JTextField textDuracion = new JTextField();
-        textDuracion.setBounds(175, 340, 150, 25);
-        textDuracion.setEditable(false);
-        panel.add(textDuracion);
+	public JPanel getPanel() {
+		return panel;
+	}
 
-        ChangeListener changeListener = new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Date fechaInicio = (Date) datePickerInicio.getModel().getValue();
-                Date fechaFin = (Date) datePickerFin.getModel().getValue();
-                if (fechaInicio != null && fechaFin != null) {
-                    long duracion = (fechaFin.getTime() - fechaInicio.getTime()) / (1000 * 60 * 60 * 24);
-                    textDuracion.setText(String.valueOf(duracion));
-                } else {
-                    textDuracion.setText("");
-                }
-            }
-        };
-
-        datePickerInicio.getModel().addChangeListener(changeListener);
-        datePickerFin.getModel().addChangeListener(changeListener);
-
-        JLabel labelServicios = new JLabel("Servicios no incluidos");
-        labelServicios.setBounds(30, 380, 150, 25);
-        panel.add(labelServicios);
-
-        JTextArea textServicios = new JTextArea();
-        textServicios.setBounds(175, 380, 400, 100);
-        panel.add(textServicios);
-
-        JLabel labelTipoViaje = new JLabel("Tipo de viaje");
-        labelTipoViaje.setBounds(30, 500, 150, 25);
-        panel.add(labelTipoViaje);
-
-        JComboBox<String> comboTipoViaje = new JComboBox<>();
-        ArrayList<TipoViaje> tiposViaje = controlador.getTiposViaje();
-        for (TipoViaje tipoViaje : tiposViaje) {
-            comboTipoViaje.addItem(tipoViaje.getDescripcion());
-        }
-        comboTipoViaje.setBounds(175, 500, 190, 25);
-        panel.add(comboTipoViaje);
-
-        JLabel labelPais = new JLabel("País destino");
-        labelPais.setBounds(30, 540, 150, 25);
-        panel.add(labelPais);
-
-        JComboBox<String> comboPais = new JComboBox<>();
-        ArrayList<Pais> paises = controlador.mostrarPaises();
-        for (Pais pais : paises) {
-            comboPais.addItem(pais.getNombre());
-        }
-        comboPais.setBounds(175, 540, 190, 25);
-        panel.add(comboPais);
-
-        JButton btnConfirmar = new JButton("Confirmar");
-        btnConfirmar.setBounds(120, 600, 150, 25);
-        btnConfirmar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Pendiente hacer que se guarden los datos en la base de datos
-            }
-        });
-        panel.add(btnConfirmar);
-
-        JButton btnCancelar = new JButton("Cancelar");
-        btnCancelar.setBounds(400, 600, 150, 25);
-        btnCancelar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Boton de cancelar que lleve a donde haga falta
-                frame.gotoViajes();
-            }
-        });
-        panel.add(btnCancelar);
-    }
-
-    public JPanel getPanel() {
-        return panel;
-    }
-
-    @SuppressWarnings("serial")
+	/**
+	 * Clase que permite formatear las fechas en el formato yyyy-MM-dd
+	 */
+	@SuppressWarnings("serial")
 	class DateLabelFormatter extends AbstractFormatter {
-        private String datePattern = "yyyy-MM-dd";
-        private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+		private String datePattern = "yyyy-MM-dd";
+		private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
 
-        @Override
-        public Object stringToValue(String text) throws ParseException {
-            return dateFormatter.parse(text);
-        }
+		// Convierte un texto en un objeto de tipo Calendar
+		@Override
+		public Object stringToValue(String text) throws ParseException {
+			return dateFormatter.parse(text);
+		}
 
-        @Override
-        public String valueToString(Object value) {
-            if (value != null) {
-                Calendar cal = (Calendar) value;
-                return dateFormatter.format(cal.getTime());
-            }
-            return "";
-        }
-    }
-
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame("Nuevo Viaje Test");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(900, 700);
-//
-//        ArrayList<JPanel> paneles = new ArrayList<>();
-//        ViajesErrekamari viajesErrekamari = new ViajesErrekamari();
-//        NuevoViaje nuevoViaje = new NuevoViaje(paneles, viajesErrekamari);
-//
-//        frame.add(nuevoViaje.getPanel());
-//        frame.setVisible(true);
-//    }
+		// Cuando se selecciona una fecha, se muestra en el campo de texto
+		@Override
+		public String valueToString(Object value) {
+			if (value != null) {
+				Calendar cal = (Calendar) value;
+				return dateFormatter.format(cal.getTime());
+			}
+			return "";
+		}
+	}
 }
